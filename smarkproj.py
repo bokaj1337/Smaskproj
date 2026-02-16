41b70aa7f0101f03553d6d33757118b8eb3dc16d
3lvisp <ellisnilsson@gmail.com>
2026-02-16 14:11:19 +0100

    Add files via upload

---
+++ smarkproj.py
@@ -0,0 +1,109 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+
+raw = pd.read_csv("training_data_VT2026.csv")
+
+def remove_constant_data(df):
+    constant_columns = [col for col in df.columns if df[col].nunique() == 1]
+    print(f"Dropping constant columns: {constant_columns}")
+    return df.drop(constant_columns, axis=1)    
+
+def process_data(df, scaler=StandardScaler):
+    '''
+    Makes stock_demand binary and scales data.
+    '''
+    out = df.replace({"low_bike_demand": 0, "high_bike_demand": 1}, inplace=True)
+    for column in out.columns:
+        out[column] = scaler.fit_transform(out[column])
+    return out
+
+fil = remove_constant_data(raw)
+fil = process_data(fil)
+
+
+def plot_hourly_avg(dataframe):
+    hourly_avg = dataframe.groupby("hour_of_day")[["temp","dew","humidity","precip","snow","snowdepth","windspeed","cloudcover","visibility","increase_stock"]].mean().reset_index()
+    plt.figure(figsize=(12, 8))
+    plt.plot(hourly_avg["hour_of_day"], hourly_avg["temp"], label="Temperature")
+    plt.plot(hourly_avg["hour_of_day"], hourly_avg["dew"], label="Dew Point")
+    plt.plot(hourly_avg["hour_of_day"], hourly_avg["humidity"], label="Humidity")
+    plt.plot(hourly_avg["hour_of_day"], hourly_avg["precip"], label="Precipitation")
+    plt.plot(hourly_avg["hour_of_day"], hourly_avg["snow"], label="Snow")
+    plt.plot(hourly_avg["hour_of_day"], hourly_avg["snowdepth"], label="Snow Depth")
+    plt.plot(hourly_avg["hour_of_day"], hourly_avg["windspeed"], label="Wind Speed")
+    plt.plot(hourly_avg["hour_of_day"], hourly_avg["cloudcover"], label="Cloud Cover")
+    plt.plot(hourly_avg["hour_of_day"], hourly_avg["visibility"], label="Visibility")
+    plt.xlabel("Hour of Day")
+    plt.ylabel("Average Value")
+    plt.title("Average Weather Conditions by Hour of Day")
+    plt.legend()
+    plt.grid()
+    plt.show()
+# plot_hourly_avg(cleaned_fil)
+
+def plot_weekday_avg():
+    weekday_avg = fil.groupby("day_of_week")[["temp","dew","humidity","precip","snow","snowdepth","windspeed","cloudcover","visibility"]].mean().reset_index()
+    plt.figure(figsize=(12, 8))
+    plt.plot(weekday_avg["day_of_week"], weekday_avg["temp"], label="Temperature")
+    plt.plot(weekday_avg["day_of_week"], weekday_avg["dew"], label="Dew Point")
+    plt.plot(weekday_avg["day_of_week"], weekday_avg["humidity"], label="Humidity")
+    plt.plot(weekday_avg["day_of_week"], weekday_avg["precip"], label="Precipitation")
+    plt.plot(weekday_avg["day_of_week"], weekday_avg["snow"], label="Snow")
+    plt.plot(weekday_avg["day_of_week"], weekday_avg["snowdepth"], label="Snow Depth")
+    plt.plot(weekday_avg["day_of_week"], weekday_avg["windspeed"], label="Wind Speed")
+    plt.plot(weekday_avg["day_of_week"], weekday_avg["cloudcover"], label="Cloud Cover")
+    plt.plot(weekday_avg["day_of_week"], weekday_avg["visibility"], label="Visibility")
+    plt.xlabel("Day of Week")
+    plt.ylabel("Average Value")
+    plt.title("Average Weather Conditions by Day of Week")
+    plt.legend()
+    plt.grid()
+    plt.show()
+# plot_weekday_avg()
+
+def plot_monthly_avg():
+    monthly_avg = fil.groupby("month")[["temp","dew","humidity","precip","snow","snowdepth","windspeed","cloudcover","visibility"]].mean().reset_index()
+    plt.figure(figsize=(12, 8))
+    plt.plot(monthly_avg["month"], monthly_avg["temp"], label="Temperature")
+    plt.plot(monthly_avg["month"], monthly_avg["dew"], label="Dew Point")
+    plt.plot(monthly_avg["month"], monthly_avg["humidity"], label="Humidity")
+    plt.plot(monthly_avg["month"], monthly_avg["precip"], label="Precipitation")
+    plt.plot(monthly_avg["month"], monthly_avg["snow"], label="Snow")
+    plt.plot(monthly_avg["month"], monthly_avg["snowdepth"], label="Snow Depth")
+    plt.plot(monthly_avg["month"], monthly_avg["windspeed"], label="Wind Speed")
+    plt.plot(monthly_avg["month"], monthly_avg["cloudcover"], label="Cloud Cover")
+    plt.plot(monthly_avg["month"], monthly_avg["visibility"], label="Visibility")
+    plt.xlabel("Month")
+    plt.ylabel("Average Value")
+    plt.title("Average Weather Conditions by Month")
+    plt.legend()
+    plt.grid()
+    plt.show()
+# plot_monthly_avg()
+
+def plot_temporal_demand():
+    fig, (ax1, ax2, ax3) = plt.subplots(3)
+
+    fig.suptitle('Vertically stacked subplots')
+
+    hourly = fil.groupby("hour_of_day")["increase_stock"].mean().reset_index()
+    ax1.plot(hourly["hour_of_day"], hourly["increase_stock"])
+    ax1.set_title("Demand over hours of day")
+
+    weekday = fil.groupby("day_of_week")["increase_stock"].mean().reset_index()  
+    ax2.plot(weekday["day_of_week"], weekday["increase_stock"])  
+    ax2.set_title("Demand over day of week") 
+
+    monthly = fil.groupby("month")["increase_stock"].mean().reset_index()  
+    ax3.plot(monthly["month"], monthly["increase_stock"]) 
+    ax3.set_title("Demand over month")
+    plt.show()    
+
+# plot_temporal_demand()
+
+
+
+
+
