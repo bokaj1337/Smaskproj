f3c76d5f5964e23e02b9abf5680856b4364217bc
Ellis Nilsson <ellisnilsson@gmail.com>
2026-02-16 14:46:45 +0100

    Fixade process_data, tog bort snow från plots

--- smarkproj.py
+++ smarkproj.py
@@ -10,27 +10,28 @@ def remove_constant_data(df):
     print(f"Dropping constant columns: {constant_columns}")
     return df.drop(constant_columns, axis=1)    
 
-def process_data(df, scaler=StandardScaler):
+def process_data(df, scaler=StandardScaler()):
     '''
     Makes stock_demand binary and scales data.
     '''
-    out = df.replace({"low_bike_demand": 0, "high_bike_demand": 1}, inplace=True)
-    for column in out.columns:
-        out[column] = scaler.fit_transform(out[column])
+    out = df.replace({"low_bike_demand": 0, "high_bike_demand": 1})
+    numerical_columns = ["temp","dew","humidity","precip","snowdepth","windspeed","cloudcover","visibility","increase_stock"]
+    for col in numerical_columns:
+        out[col] = scaler.fit_transform(out[[col]])
     return out
 
+
 fil = remove_constant_data(raw)
-fil = process_data(fil)
+cleaned_fil = process_data(fil)
 
 
 def plot_hourly_avg(dataframe):
-    hourly_avg = dataframe.groupby("hour_of_day")[["temp","dew","humidity","precip","snow","snowdepth","windspeed","cloudcover","visibility","increase_stock"]].mean().reset_index()
+    hourly_avg = dataframe.groupby("hour_of_day")[["temp","dew","humidity","precip","snowdepth","windspeed","cloudcover","visibility","increase_stock"]].mean().reset_index()
     plt.figure(figsize=(12, 8))
     plt.plot(hourly_avg["hour_of_day"], hourly_avg["temp"], label="Temperature")
     plt.plot(hourly_avg["hour_of_day"], hourly_avg["dew"], label="Dew Point")
     plt.plot(hourly_avg["hour_of_day"], hourly_avg["humidity"], label="Humidity")
     plt.plot(hourly_avg["hour_of_day"], hourly_avg["precip"], label="Precipitation")
-    plt.plot(hourly_avg["hour_of_day"], hourly_avg["snow"], label="Snow")
     plt.plot(hourly_avg["hour_of_day"], hourly_avg["snowdepth"], label="Snow Depth")
     plt.plot(hourly_avg["hour_of_day"], hourly_avg["windspeed"], label="Wind Speed")
     plt.plot(hourly_avg["hour_of_day"], hourly_avg["cloudcover"], label="Cloud Cover")
@@ -41,16 +42,15 @@ def plot_hourly_avg(dataframe):
     plt.legend()
     plt.grid()
     plt.show()
-# plot_hourly_avg(cleaned_fil)
+plot_hourly_avg(cleaned_fil)
 
 def plot_weekday_avg():
-    weekday_avg = fil.groupby("day_of_week")[["temp","dew","humidity","precip","snow","snowdepth","windspeed","cloudcover","visibility"]].mean().reset_index()
+    weekday_avg = fil.groupby("day_of_week")[["temp","dew","humidity","precip","snowdepth","windspeed","cloudcover","visibility"]].mean().reset_index()
     plt.figure(figsize=(12, 8))
     plt.plot(weekday_avg["day_of_week"], weekday_avg["temp"], label="Temperature")
     plt.plot(weekday_avg["day_of_week"], weekday_avg["dew"], label="Dew Point")
     plt.plot(weekday_avg["day_of_week"], weekday_avg["humidity"], label="Humidity")
     plt.plot(weekday_avg["day_of_week"], weekday_avg["precip"], label="Precipitation")
-    plt.plot(weekday_avg["day_of_week"], weekday_avg["snow"], label="Snow")
     plt.plot(weekday_avg["day_of_week"], weekday_avg["snowdepth"], label="Snow Depth")
     plt.plot(weekday_avg["day_of_week"], weekday_avg["windspeed"], label="Wind Speed")
     plt.plot(weekday_avg["day_of_week"], weekday_avg["cloudcover"], label="Cloud Cover")
@@ -64,13 +64,12 @@ def plot_weekday_avg():
 # plot_weekday_avg()
 
 def plot_monthly_avg():
-    monthly_avg = fil.groupby("month")[["temp","dew","humidity","precip","snow","snowdepth","windspeed","cloudcover","visibility"]].mean().reset_index()
+    monthly_avg = fil.groupby("month")[["temp","dew","humidity","precip","snowdepth","windspeed","cloudcover","visibility"]].mean().reset_index()
     plt.figure(figsize=(12, 8))
     plt.plot(monthly_avg["month"], monthly_avg["temp"], label="Temperature")
     plt.plot(monthly_avg["month"], monthly_avg["dew"], label="Dew Point")
     plt.plot(monthly_avg["month"], monthly_avg["humidity"], label="Humidity")
     plt.plot(monthly_avg["month"], monthly_avg["precip"], label="Precipitation")
-    plt.plot(monthly_avg["month"], monthly_avg["snow"], label="Snow")
     plt.plot(monthly_avg["month"], monthly_avg["snowdepth"], label="Snow Depth")
     plt.plot(monthly_avg["month"], monthly_avg["windspeed"], label="Wind Speed")
     plt.plot(monthly_avg["month"], monthly_avg["cloudcover"], label="Cloud Cover")
