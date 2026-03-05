5b26e5fc7ed35ab7b34fca9b702d91fa97460a43
bokaj1337 <jakobkristenson@gmail.com>
2026-03-05 15:18:19 +0100

    pullllaaa

--- logisticregression.py
+++ logisticregression.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split,cross_val_score, StratifiedKFold, GridSearchCV
-from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix, f1_score
 from load_data import get_ready_data
 
 cleaned_fil = get_ready_data()
@@ -17,11 +17,13 @@ param_grid = {'C': [0.1, 1 ,5,10,100], 'penalty': ['l1','l2'], 'class_weight': [
 
 skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=69)
 
-grid_search = GridSearchCV(estimator=model, param_grid=param_grid, cv=skf, scoring='f1_macro', verbose=1)
+grid_search = GridSearchCV(estimator=model, param_grid=param_grid, cv=skf, scoring='f1', verbose=1)
 
 grid_search.fit(X_train, Y_train)
 
 print(grid_search.best_params_)
+print(grid_search.cv_results_['mean_test_score'][grid_search.best_index_])
+print(grid_search.cv_results_['std_test_score'][grid_search.best_index_])
 
 best_model = grid_search.best_estimator_
 Y_pred = best_model.predict(X_test)
