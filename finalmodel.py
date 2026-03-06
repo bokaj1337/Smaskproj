5d8a45457ee80ee7444a5f2037505351af0de12d
bokaj1337 <jakobkristenson@gmail.com>
2026-03-06 11:14:14 +0100

    gooooooonn

--- finalmodel.py
+++ finalmodel.py
@@ -8,6 +8,9 @@ cleaned_fil = get_ready_data()
 
 predicted = knn_model.predict(cleaned_fil)
 
+
+# print(knn_model.predict_proba(cleaned_fil))
+
 # with open("predictions.csv", "w") as f:
 #     f.write(",".join(map(str, predicted)))
 
