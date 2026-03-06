e7e03d08bf3e2efd132af6b901a89cfd1a45d8cb
bokaj1337 <jakobkristenson@gmail.com>
2026-03-06 10:31:21 +0100

    walla

---
+++ finalmodel.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+from load_data_real import get_ready_data
+from knn import best_model as knn_model
+
+
+cleaned_fil = get_ready_data()
+
+predicted = knn_model.predict(cleaned_fil)
+
+print(predicted)
+
+
